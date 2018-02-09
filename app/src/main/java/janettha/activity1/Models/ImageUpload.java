326b7a3144b152f51256a8c4e42ca0b24febcd0b
Janeth Salas Gomez <janethsalasgomez@gmail.com>
2018-02-09 00:53:23 -0600

    ImageUpload
        private String nombre;
        private int id;
        private String sexo;
        private String url;

--- app/src/main/java/janettha/activity1/Models/ImageUpload.java
+++ app/src/main/java/janettha/activity1/Models/ImageUpload.java
@@ -5,28 +5,31 @@ package janettha.activity1.Models;
  */
 
 public class ImageUpload {
-    public String feel;
-    public int id;
-    public String name;
-    public String url;
+    private String nombre;
+    private int id;
+    private String sexo;
+    private String url;
 
-    public String getName() {
-        return name;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getFeel() { return feel; }
-
-    public int getId() { return id; }
-
-    public ImageUpload(String name, String feel, int id) {
-        this.name = name;
-        this.feel = feel;
+    public ImageUpload(String nombre, int id, String sexo, String url) {
+        this.nombre = nombre;
         this.id = id;
+        this.sexo = sexo;
+        this.url = url;
     }
 
-    public ImageUpload(){}
+    public String getNombre() {       return nombre;    }
+
+    public void setNombre(String nombre) {        this.nombre = nombre;    }
+
+    public int getId() {        return id;    }
+
+    public void setId(int id) {        this.id = id;    }
+
+    public String getSexo() {        return sexo;    }
+
+    public void setSexo(String sexo) {        this.sexo = sexo;    }
+
+    public String getUrl() {        return url;    }
+
+    public void setUrl(String url) {        this.url = url;    }
 }
