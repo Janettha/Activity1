7cc0cad50eec4934a5bff0fdc223c7abb7f7c8d5
janettha <janethsalasgomez@gmail.com>
2018-02-28 00:22:47 -0600

    CustomDialog en actividad 2 - parte 2 record / stopped / saved Firebase

--- app/src/main/java/janettha/activity1/Act2/Activity2.java
+++ app/src/main/java/janettha/activity1/Act2/Activity2.java
@@ -2,21 +2,35 @@ package janettha.activity1.Act2;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
-import android.app.DialogFragment;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DialogTitle;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewDebug;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.ui.phone.CompletableProgressDialog;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import github.hellocsl.cursorwheel.CursorWheelLayout;
@@ -32,33 +46,37 @@ public class Activity2 extends AppCompatActivity implements CursorWheelLayout.On
     View v;
     ImageView EmocionDialog;
     TextView NameEmocionDialog;
-    Button buttonRecord, buttonPlay;
+    Button buttonRecord, buttonStop;
     Dialog dialog;
     boolean DialogFlag=false;
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String mFileName = null;
+
+    private RecordButton mRecordButton;
+    private MediaRecorder mRecorder;
+
+    private StorageReference mStorage;
+    private CompletableProgressDialog mProgress;
+
+    private final String extension = ".3gp";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
+
         //View rootView = R.layout.activity_activity2.inflate(R.layout.fragment_preactivity, container, false);
         v = initViews();
         loadData("f",v);
         wheel_img.setOnMenuSelectedListener(this);
+                    /* Recording audio FIREBASE */
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath()+"/";
+        //mFileName += "/recorded_audio.3gp";
 
-        Button buttonDialog = (Button) findViewById(R.id.EmocionDialog);
-        buttonDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int posicion = wheel_img.getSelectedPosition();
-                MyCustomAlertDialog(posicion);
-                /*
-                Toast.makeText(getApplicationContext(), "ID wheel: "+wheel_img.getSelectedPosition(), Toast.LENGTH_SHORT).show();
-                if(DialogFlag == true) {
-                    MyCustomAlertDialog(11);
-                }
-                */
-            }
-        });
 
 
     }
@@ -79,6 +97,7 @@ public class Activity2 extends AppCompatActivity implements CursorWheelLayout.On
     @Override
     public void onItemSelected(CursorWheelLayout parent, View view, int pos) {
         //TextView t = (TextView) findViewById(R.id.id_wheel_menu_center_item);
+        final Button buttonDialog = (Button) findViewById(R.id.EmocionDialog);
         if(parent.getId() == R.id.wheel){
             Toast.makeText(getBaseContext(), listImg.get(pos).getName(), Toast.LENGTH_SHORT).show();
             //t.setText(listImg.get(pos).getName());
@@ -90,6 +109,22 @@ public class Activity2 extends AppCompatActivity implements CursorWheelLayout.On
                 public void onItemSelected(CursorWheelLayout parent, View view, int pos) {
                     DialogFlag = true;
                     Toast.makeText(Activity2.this, "Top Menu click position:" + pos + " Dialog: "+DialogFlag, Toast.LENGTH_SHORT).show();
+
+                    final int posicion = wheel_img.getSelectedPosition();
+
+                    mFileName += listImg.get(wheel_img.getSelectedPosition()).getId() + "_" + listImg.get(wheel_img.getSelectedPosition()).getName() + extension;
+
+
+                    buttonDialog.setVisibility(View.VISIBLE);
+                    buttonDialog.setText(listImg.get(posicion).getName());
+                    buttonDialog.setBackgroundColor(Color.parseColor(listImg.get(posicion).getColorB()));
+                    buttonDialog.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            //
+                            MyCustomAlertDialog(posicion);
+                        }
+                    });
                 }
             });
 
@@ -104,7 +139,7 @@ public class Activity2 extends AppCompatActivity implements CursorWheelLayout.On
         Toast.makeText(this,"Dialog", Toast.LENGTH_SHORT).show();
 
         buttonRecord = (Button) dialog.findViewById(R.id.btnRecord);
-        buttonPlay = (Button) dialog.findViewById(R.id.btnStop);
+        buttonStop = (Button) dialog.findViewById(R.id.btnStop);
         EmocionDialog = (ImageView) dialog.findViewById(R.id.imgEmocion);
         NameEmocionDialog = (TextView) dialog.findViewById(R.id.nameEmocion);
 
@@ -114,23 +149,164 @@ public class Activity2 extends AppCompatActivity implements CursorWheelLayout.On
                 .into(EmocionDialog);
         NameEmocionDialog.setText(listImg.get(pos).getName());
         buttonRecord.setEnabled(true);
-        buttonPlay.setEnabled(true);
+        buttonStop.setEnabled(false);
 
+        /*
         buttonRecord.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"Recording...", Toast.LENGTH_SHORT).show();
             }
         });
+        */
+        // Permisos
+        if(checkPermissionFromDevice()) {
 
-        buttonPlay.setOnClickListener(new View.OnClickListener(){
+            /*
+            buttonRecord.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        startRecording();
+                        Toast.makeText(getApplicationContext(), "Recording...", Toast.LENGTH_SHORT).show();
+                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                        stopRecording();
+                        Toast.makeText(getApplicationContext(), "Record Stopped...", Toast.LENGTH_SHORT).show();
+                    }
+
+                    return false;
+                }
+            });
+            */
+
+            buttonRecord.setEnabled(true);
+            buttonRecord.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                        //mRecorder.prepare();
+                        //mRecorder.start();
+                    buttonRecord.setEnabled(false);
+                    buttonStop.setEnabled(true);
+                    buttonRecord.setEnabled(false);
+                    startRecording();
+                    Toast.makeText(Activity2.this, "Recording...", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else{
+            requestPermissions();
+
+        }
+
+
+        buttonStop.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Stop...", Toast.LENGTH_SHORT).show();
-                dialog.cancel();
-                DialogFlag = false;
+                buttonRecord.setEnabled(true);
+                buttonStop.setEnabled(false);
+                stopRecording();
+                Toast.makeText(getApplicationContext(), "Record Stopped...", Toast.LENGTH_SHORT).show();
+
+                //DialogFlag = false;
             }
         });
         dialog.show();
     }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.RECORD_AUDIO
+        }, REQUEST_RECORD_AUDIO_PERMISSION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    Toast.makeText(this, "Permission Granted.", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, "Permission Denied.", Toast.LENGTH_SHORT).show();
+            }
+            break;
+        }
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        //mRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+
+        uploadAudio();
+    }
+
+    private void uploadAudio() {
+        String path = listImg.get(wheel_img.getSelectedPosition()).getId() + "_" + listImg.get(wheel_img.getSelectedPosition()).getName() + extension;
+        StorageReference filepath = mStorage.child("Audio").child(path);
+        Uri uri = Uri.fromFile(new File(mFileName));
+        filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getApplicationContext(),"Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    public class RecordButton extends android.support.v7.widget.AppCompatButton {
+
+        boolean mStartRecording = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                    setText("Stop recording");
+                } else {
+                    setText("Start recording");
+                }
+                mStartRecording = !mStartRecording;
+            }
+        };
+
+        public RecordButton(Context ctx) {
+            super(ctx);
+            setText("Start recording");
+            setOnClickListener(clicker);
+        }
+    }
+
+    private boolean checkPermissionFromDevice(){
+        int write_external_storage_result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int record_audio_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        return (write_external_storage_result == PackageManager.PERMISSION_GRANTED &&
+                record_audio_result == PackageManager.PERMISSION_GRANTED);
+    }
+
 }
